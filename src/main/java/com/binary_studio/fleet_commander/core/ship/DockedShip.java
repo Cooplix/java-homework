f545a5598650fc70d649bc9f2b4c4731bb192e9c
Vadym <vadikdeputat@gmail.com>
2021-05-08 15:17:34 +0200

    finished DockedShip

--- src/main/java/com/binary_studio/fleet_commander/core/ship/DockedShip.java
+++ src/main/java/com/binary_studio/fleet_commander/core/ship/DockedShip.java
@@ -9,27 +9,111 @@ import com.binary_studio.fleet_commander.core.subsystems.contract.DefenciveSubsy
 
 public final class DockedShip implements ModularVessel {
 
+	private String name;
+
+	private PositiveInteger shieldHP;
+
+	private PositiveInteger hullHP;
+
+	private PositiveInteger powergridOutput;
+
+	private PositiveInteger capacitorAmount;
+
+	private PositiveInteger capacitorRechargeRate;
+
+	private PositiveInteger speed;
+
+	private PositiveInteger siz;
+
+	private AttackSubsystem attackSubsystem;
+
+	private DefenciveSubsystem defenciveSubsystem;
+
+	public DockedShip(String name, PositiveInteger shieldHP, PositiveInteger hullHP, PositiveInteger powergridOutput,
+			PositiveInteger capacitorAmount, PositiveInteger capacitorRechargeRate, PositiveInteger speed,
+			PositiveInteger siz) {
+		this.name = name;
+		this.shieldHP = shieldHP;
+		this.hullHP = hullHP;
+		this.powergridOutput = powergridOutput;
+		this.capacitorAmount = capacitorAmount;
+		this.capacitorRechargeRate = capacitorRechargeRate;
+		this.speed = speed;
+		this.siz = siz;
+	}
+
 	public static DockedShip construct(String name, PositiveInteger shieldHP, PositiveInteger hullHP,
 			PositiveInteger powergridOutput, PositiveInteger capacitorAmount, PositiveInteger capacitorRechargeRate,
 			PositiveInteger speed, PositiveInteger size) {
-		// TODO: Ваш код здесь :)
-		return null;
+
+		return new DockedShip(name, shieldHP, hullHP, powergridOutput, capacitorAmount, capacitorRechargeRate, speed,
+				size);
 	}
 
 	@Override
 	public void fitAttackSubsystem(AttackSubsystem subsystem) throws InsufficientPowergridException {
-		// TODO: Ваш код здесь :)
+		if (subsystem == null) {
+			attackSubsystem = null;
+		}
+		else {
+			if (defenciveSubsystem == null) {
+				if (subsystem.getPowerGridConsumption().value() > powergridOutput.value()) {
+					int missingPowerGrid = subsystem.getPowerGridConsumption().value() - powergridOutput.value();
+					throw new InsufficientPowergridException(missingPowerGrid);
+				}
+			}
+			else {
+				if (subsystem.getPowerGridConsumption().value()
+						+ defenciveSubsystem.getCapacitorConsumption().value() > powergridOutput.value()) {
+					int missingPowerGrid = subsystem.getPowerGridConsumption().value()
+							+ defenciveSubsystem.getPowerGridConsumption().value() - powergridOutput.value();
+					throw new InsufficientPowergridException(missingPowerGrid);
+				}
+			}
+			attackSubsystem = subsystem;
+		}
 	}
 
 	@Override
 	public void fitDefensiveSubsystem(DefenciveSubsystem subsystem) throws InsufficientPowergridException {
-		// TODO: Ваш код здесь :)
+		if (subsystem == null) {
+			defenciveSubsystem = null;
+		}
+		else {
+			if (attackSubsystem == null) {
+				if (subsystem.getPowerGridConsumption().value() > powergridOutput.value()) {
+					int missingPowerGrid = subsystem.getPowerGridConsumption().value() - powergridOutput.value();
+					throw new InsufficientPowergridException(missingPowerGrid);
+				}
+			}
+			else {
+				if (subsystem.getPowerGridConsumption().value()
+						+ attackSubsystem.getCapacitorConsumption().value() > powergridOutput.value()) {
+					int missingPowerGrid = subsystem.getPowerGridConsumption().value()
+							+ attackSubsystem.getPowerGridConsumption().value() - powergridOutput.value();
+					throw new InsufficientPowergridException(missingPowerGrid);
+				}
+			}
+			defenciveSubsystem = subsystem;
+		}
 
 	}
 
 	public CombatReadyShip undock() throws NotAllSubsystemsFitted {
-		// TODO: Ваш код здесь :)
-		return null;
+		if (attackSubsystem == null) {
+			throw NotAllSubsystemsFitted.attackMissing();
+		}
+		else if (defenciveSubsystem == null) {
+			if (attackSubsystem == null) {
+				throw NotAllSubsystemsFitted.bothMissing();
+			}
+			else {
+				throw NotAllSubsystemsFitted.defenciveMissing();
+			}
+		}
+		else {
+			return new CombatReadyShip();
+		}
 	}
 
 }
